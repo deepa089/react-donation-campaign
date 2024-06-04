b51bee4d5375e6c954187059189e29b71647b82b
deepa robisina <robisina.deepa@bracits.com>
2024-06-04 10:11:20 +0600

    folder create done

---
+++ src/components/DonationDetails/DonationDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DonationDetails = () => {
+    return (
+        <div>
+            <h2>Donation Details</h2>
+        </div>
+    );
+};
+
+export default DonationDetails;
---
+++ src/components/DonationList/DonationList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DonationList = () => {
+    return (
+        <div>
+            <h2>Donation List</h2>
+        </div>
+    );
+};
+
+export default DonationList;
