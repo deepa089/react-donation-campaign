d98a2cc97d0c3397231fb1e2c1dfa74a6bf53f67
deepa robisina <robisina.deepa@bracits.com>
2024-06-23 17:30:24 +0600

    done 1

--- src/components/DonationDetails/DonationDetails.jsx
+++ src/components/DonationDetails/DonationDetails.jsx
@@ -18,24 +18,28 @@ const DonationDetails = () => {
     }
 
     return (
-        <div className='px-36 pb-32'>
-            {/* <div className='h-1/5'>
-                <img className='w-full mix-blend-multiply bg-blue-400' src={donationInfo.img}></img>
-                <div className="mix-blend-multiply bg-pink-400  -mb-10">
-                    <button>Donate</button>
-                </div>
-            </div> */} 
-            <div className="relative overflow-hidden">
-                <img src={donationInfo.img} alt="Avatar" className="object-cover w-full h-full" />
-                {/* <div className="absolute w-full py-2.5 bottom-0 inset-x-0 bg-blue-400 text-white text-xs text-center leading-4">this is a text</div> */}
-                <div className="absolute w-full py-9 bottom-0  bg-gray-500 text-white text-2xl pl-8  mix-blend-multiply">
-                    <button className='bg-[#FF444A] text-white py-4 px-6 rounded font-semibold z-10 opacity-50 hover:opacity-100' onClick={() => handleSaveDonation(`${donationInfo.id}`)}>Donate ${donationInfo.price}</button>
-                </div>
+        // <div className='px-36 pb-32'>
+        //     {/*  <div style={{ backgroundImage: `url(${rectangle_image})`, backgroundPosition: 'top', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} className="w-full h-screen rounded-lg parent">
+        //         <div className="child w-full h-1/6 flex">
+        //             <button onClick={handleDonationsList} style={{backgroundColor: `${color_dark}`}} className="text-white md:text-xl font-semibold px-3 py-2 md:px-6 md:py-4 rounded my-auto ml-9">Donate ${amount}</button>
+        //         </div>
+        //     </div> */} 
+        //     <div className="relative overflow-hidden">
+        //         <img src={donationInfo.img} alt="Avatar" className="object-cover w-full h-full" />
+        //         {/* <div className="absolute w-full py-2.5 bottom-0 inset-x-0 bg-blue-400 text-white text-xs text-center leading-4">this is a text</div> */}
+        //         <div className="absolute w-full py-9 bottom-0  bg-gray-500 text-white text-2xl pl-8  mix-blend-multiply">
+        //             <button className='bg-[#FF444A] text-white py-4 px-6 rounded font-semibold z-10 opacity-50 hover:opacity-100' onClick={() => handleSaveDonation(`${donationInfo.id}`)}>Donate ${donationInfo.price}</button>
+        //         </div>
+        //     </div>
+
+        //     <h2 className='text-3xl font-semibold py-6'>{donationInfo.title}</h2>
+        //     <p className='text-sm text-[#0b0b0bb3] text-justify'>{donationInfo.description}</p>
+
+        // </div>
+        <div>
+            <div style={{ backgroundImage: `url(${donationInfo.img})`, backgroundPosition: 'top', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} className="w-full h-full">
+
             </div>
-
-            <h2 className='text-3xl font-semibold py-6'>{donationInfo.title}</h2>
-            <p className='text-sm'>{donationInfo.description}</p>
-
         </div>
     );
 };
